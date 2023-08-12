d93483fd6de9ed8388909af545fc1544c0743714
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-08-12 20:30:56 +0530

    add hibernate configuration

--- src/main/java/lk/ijse/gdse/servlet/CustomerServlet.java
+++ src/main/java/lk/ijse/gdse/servlet/CustomerServlet.java
@@ -1,5 +1,8 @@
 package lk.ijse.gdse.servlet;
 
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReader;
 import jakarta.json.bind.Jsonb;
 import jakarta.json.bind.JsonbBuilder;
 import lk.ijse.gdse.dto.CustomerDto;
@@ -18,22 +21,22 @@ import java.sql.*;
 import java.util.regex.Pattern;
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
-
-    Connection connection;
     private final String sql = "INSERT INTO customer VALUES (?,?,?,?)";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/thogakade";
     private static final String DB_USER = "root";
     private static final String DB_PASS = "1234";
 
-
+//    private Connection connection ;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("do-post");
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection =  DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
-            System.out.println("oky");
+            Connection connection =  DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+            System.out.println("connection oky");
+            String contentType = req.getContentType();
+            System.out.println(contentType);
 
             Jsonb jsonb = JsonbBuilder.create();
             CustomerDto customerDto = jsonb.fromJson(req.getReader(), CustomerDto.class);
@@ -42,13 +45,13 @@ public class CustomerServlet extends HttpServlet {
             ps.setString(1,customerDto.getId());
             ps.setString(2,customerDto.getName());
             ps.setString(3,customerDto.getAddress());
-            ps.setDouble(4,customerDto.getSalary());
+            ps.setDouble(4, customerDto.getSalary());
             int i = ps.executeUpdate();
             if (i>0){
                 System.out.println("added oky");
+            }else {
+                System.out.println("added fail");
             }
-            System.out.println("added fail");
-
         } catch (SQLException |ClassNotFoundException| RuntimeException e) {
             e.printStackTrace();
         }
@@ -85,4 +88,40 @@ public class CustomerServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("sdggf");
     }
+//    @Override
+//    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        try {
+//            Class.forName(getServletContext().getInitParameter("mysql-driver"));
+//            String username = getServletContext().getInitParameter("db-user");
+//            String password = getServletContext().getInitParameter("db-pw");
+//            String url = getServletContext().getInitParameter("db-url");
+//            this.connection = DriverManager.getConnection(url,username,password);
+//
+//        } catch (ClassNotFoundException | SQLException ex) {
+//            throw new RuntimeException(ex);
+//        }
+//        JsonReader reader = Json.createReader(req.getReader());
+//        JsonObject jsonObject = reader.readObject();
+//        String id = jsonObject.getString("id");
+//        String name = jsonObject.getString("name");
+//        String address = jsonObject.getString("address");
+//        String salary = String.valueOf(jsonObject.getInt("salary"));
+//        try {
+//            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE customer SET name = ? , address = ? , salary = ? WHERE id = ?");
+//            preparedStatement.setObject(1,name);
+//            preparedStatement.setObject(2,address);
+//            preparedStatement.setObject(3,salary);
+//            preparedStatement.setObject(4,id);
+//            int i = preparedStatement.executeUpdate();
+//            if (i>1){
+//                System.out.println("updated");
+//                PrintWriter writer = resp.getWriter();
+//                writer.write("updated successfully");
+//            }
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+    
 }
